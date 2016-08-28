88b788112c6c330f4a6222a9ff920fe15821c549
Mark <mystycs@gmail.com>
2016-08-27 20:07:40 -0400

    added projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+   root 'projects#index', as: 'projects'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
