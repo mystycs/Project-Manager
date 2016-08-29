b4988ee28a6addd37ee030f3c763a10645b3fa1c
Mark <mystycs@gmail.com>
2016-08-28 22:23:44 -0400

    submit project after creating category

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+
 end
