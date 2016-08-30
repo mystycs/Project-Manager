122f3e5d40325d4c7c057e4ec0689bdcd7eee467
Mark <mystycs@gmail.com>
2016-08-30 16:24:38 -0400

    add tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830032216) do
+ActiveRecord::Schema.define(version: 20160830202337) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20160830032216) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "project_id"
+    t.string   "task"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
