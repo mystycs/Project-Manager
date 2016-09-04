ef9f656500d95b847648d70dc6701609315dd389
Mark <mystycs@gmail.com>
2016-09-04 17:45:32 -0400

    design/cleanup

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,15 +4,6 @@ class Task < ActiveRecord::Base
 
   validates_presence_of :task
 
-  #def count_tasks
-    # count = Task.where(["project_id = ?", projectid]).count
-    # if count = 0
-    #   @count = 100
-    # else
-    #   @count = (100/count)
-    # end
-  #end
-
   def self.filter_tasks(projectid)
     where(["project_id = ?", projectid])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
 
   validates_presence_of :full_name
 
-
-
   has_many :comments
   has_many :projects
 
