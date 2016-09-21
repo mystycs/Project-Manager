aa8c6a4a42932687994340d2433a7079cc4c0e71
Mark <mystycs@gmail.com>
2016-09-21 18:45:19 -0400

    rubocop cleanup

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
   # validates_uniqueness_of :description
 
   validates_presence_of :title, :description
-  validates_presence_of :category_ids, :message => "A category must be present"
+  validates_presence_of :category_ids, message: 'A category must be present'
 
   # accepts_nested_attributes_for :categories, :reject_if => proc { |attrs| attrs['title'].blank? }
 
